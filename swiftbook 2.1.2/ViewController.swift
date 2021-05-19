ecbb56c6ac668860ccded660e2dd2a057967cf61
yakor1 <mr.yarikk@gmail.com>
2021-05-19 14:57:47 +0300

    trying add next picker

--- swiftbook 2.1.2/ViewController.swift
+++ swiftbook 2.1.2/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  swiftbook 2.1.2
-//
-//  Created by Яков Z on 18.05.2021.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var redButtonView: UIView!
+    @IBOutlet weak var yellowButtonView: UIView!
+    @IBOutlet weak var greenButtonView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        startButton.setTitle("Start", for: .normal)
+//        startButton.setTitle("Next", for: .selected)
+        redButtonView.alpha = 0.3
+        yellowButtonView.alpha = 0.3
+        greenButtonView.alpha = 0.3
     }
-
-
+    
+    @IBAction func tappedButton(_ sender: UIButton) {
+        startButton.setTitle("Next", for: .normal)
+    }
+    
+    enum stackView {
+        case redButtonView
+        case yelloWButtonView
+        case greenButtonView
+    }
 }
 
