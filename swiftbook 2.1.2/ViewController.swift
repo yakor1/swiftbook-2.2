ddc2132cc4a803741962a891c7fbaf6602594d57
yakor1 <mr.yarikk@gmail.com>
2021-05-19 21:02:34 +0300

    fix circle

--- swiftbook 2.1.2/ViewController.swift
+++ swiftbook 2.1.2/ViewController.swift
@@ -3,30 +3,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var redButtonView: UIView!
     @IBOutlet weak var yellowButtonView: UIView!
     @IBOutlet weak var greenButtonView: UIView!
-    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var startButton: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        startButton.setTitle("Start", for: .normal)
-//        startButton.setTitle("Next", for: .selected)
         redButtonView.alpha = 0.3
         yellowButtonView.alpha = 0.3
         greenButtonView.alpha = 0.3
     }
     
-    @IBAction func tappedButton(_ sender: UIButton) {
-        startButton.setTitle("Next", for: .normal)
-    }
-    
-    enum stackView {
-        case redButtonView
-        case yelloWButtonView
-        case greenButtonView
+    @IBAction func tappedNextButton() {
+
     }
 }
 
